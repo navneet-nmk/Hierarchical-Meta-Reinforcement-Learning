dd0adfe7d0ee082eefca924ce9fbbb58cec8e4cb
ayir <navneet.nmk@gmail.com>
2019-01-23 19:19:11 +0100

    Added Torch RL Algorithm

--- maml_rl/policies/empowerment_skills.py
+++ maml_rl/policies/empowerment_skills.py
@@ -3,8 +3,9 @@ import torch
 import torch.nn.functional as F
 from collections import deque
 import gtimer as gt
-import json
-from rlkit.torch.sac.sac import SoftActorCritic
+import rlkit.rlkit.torch.pytorch_util as ptu
+import torch.optim as optim
+from rlkit.rlkit.torch.torch_rl_algorithm import TorchRLAlgorithm
 from tensorboardX import SummaryWriter
 
 
@@ -13,15 +14,26 @@ print("Using the torch version : ", torch.__version__)
 EPS = 1E-6
 
 
-class EmpowermentSkills(object):
+class EmpowermentSkills(TorchRLAlgorithm):
 
     def __init__(self,
                  env,
                  policy,
                  discriminator,
-                 q_value_function,
+                 q_value_function_1,
+                 q_value_function_2,
                  value_function,
                  pool,
+
+                 policy_lr=1e-3,
+                 qf_lr=1e-3,
+                 vf_lr=1e-3,
+                 disc_lr=1e-3,
+                 policy_mean_reg_weight=1e-3,
+                 policy_std_reg_weight=1e-3,
+                 policy_pre_activation_weight=0.,
+                 optimizer_class=optim.Adam,
+
                  plotter=None,
                  lr=3E-3,
                  scale_entropy=1,
@@ -33,13 +45,59 @@ class EmpowermentSkills(object):
                  best_skill_n_rollouts=10,
                  learn_p_z=False,
                  include_actions=False,
-                 add_p_z=True
+                 add_p_z=True,
+
+                 train_policy_with_reparameterization=True,
+                 soft_target_tau=1e-2,
+                 policy_update_period=1,
+                 target_update_period=1,
+                 render_eval_paths=False,
+                 eval_deterministic=True,
+
+                 eval_policy=None,
+                 exploration_policy=None,
+
+                 use_automatic_entropy_tuning=True,
+                 target_entropy=None,
+                 **kwargs
                  ):
+
+        """
+
+        Uses Soft Actor critic with the twin architecture from TD3 (increased stability in the training)
+
+        :param env:
+        :param policy:
+        :param discriminator:
+        :param q_value_function:
+        :param value_function:
+        :param pool:
+        :param plotter:
+        :param lr:
+        :param scale_entropy:
+        :param discount:
+        :param tau:
+        :param num_skills:
+        :param save_full_state:
+        :param find_best_skill_interval:
+        :param best_skill_n_rollouts:
+        :param learn_p_z:
+        :param include_actions:
+        :param add_p_z:
+        """
+
+        super().__init__(
+            env=env,
+            exploration_policy=exploration_policy or policy,
+            eval_policy=eval_policy,
+            **kwargs
+        )
 
         self.env = env
         self.policy = policy
         self.discriminator = discriminator
-        self.qvf = q_value_function
+        self.qvf = q_value_function_1
+        self.qvf_2 = q_value_function_2
         self.vf = value_function
         self.pool = pool
         self.plotter = plotter
@@ -57,33 +115,64 @@ class EmpowermentSkills(object):
         self.p_z = np.full(num_skills, 1.0 / num_skills)
         self.writer = SummaryWriter()
 
-        """
-                Args:
-                    base_kwargs (dict): dictionary of base arguments that are directly
-                        passed to the base `RLAlgorithm` constructor.
-                    env (`rllab.Env`): rllab environment object.
-                    policy: (`rllab.NNPolicy`): A policy function approximator.
-                    discriminator: (`rllab.NNPolicy`): A discriminator for z.
-                    qf (`ValueFunction`): Q-function approximator.
-                    vf (`ValueFunction`): Soft value function approximator.
-                    pool (`PoolBase`): Replay buffer to add gathered samples to.
-                    plotter (`QFPolicyPlotter`): Plotter instance to be used for
-                        visualizing Q-function during training.
-                    lr (`float`): Learning rate used for the function approximators.
-                    scale_entropy (`float`): Scaling factor for entropy.
-                    discount (`float`): Discount factor for Q-function updates.
-                    tau (`float`): Soft value function target update weight.
-                    num_skills (`int`): Number of skills/options to learn.
-                    save_full_state (`bool`): If True, save the full class in the
-                        snapshot. See `self.get_snapshot` for more information.
-                    find_best_skill_interval (`int`): How often to recompute the best
-                        skill.
-                    best_skill_n_rollouts (`int`): When finding the best skill, how
-                        many rollouts to do per skill.
-                    include_actions (`bool`): Whether to pass actions to the
-                        discriminator.
-                    add_p_z (`bool`): Whether th include log p(z) in the pseudo-reward.
-        """
+        self.policy_lr = policy_lr
+        self.qvf_lr = qf_lr
+        self.vf_lr = vf_lr
+        self.disc_lr = disc_lr
+
+        self.optim = optimizer_class
+
+        self.soft_target_tau = soft_target_tau
+        self.policy_update_period = policy_update_period
+        self.target_update_period = target_update_period
+        self.policy_mean_reg_weight = policy_mean_reg_weight
+        self.policy_std_reg_weight = policy_std_reg_weight
+        self.policy_pre_activation_weight = policy_pre_activation_weight
+        self.train_policy_with_reparameterization = (
+            train_policy_with_reparameterization
+        )
+
+        self.use_automatic_entropy_tuning = use_automatic_entropy_tuning
+        if self.use_automatic_entropy_tuning:
+            if target_entropy:
+                self.target_entropy = target_entropy
+            else:
+                self.target_entropy = -np.prod(self.env.action_space.shape).item()  # heuristic value from Tuomas
+            self.log_alpha = ptu.zeros(1, requires_grad=True)
+            self.alpha_optimizer = optimizer_class(
+                [self.log_alpha],
+                lr=policy_lr,
+            )
+
+        self.render_eval_paths = render_eval_paths
+
+        # Define the target value function
+        self.target_vf = self.vf.copy()
+        # Define the loss functions for the critic and state value function
+        self.qf_criterion = torch.nn.MSELoss()
+        self.vf_criterion = torch.nn.MSELoss()
+
+        # Define the optimizers
+        self.policy_optimizer = optimizer_class(
+            self.policy.parameters(),
+            lr=policy_lr,
+        )
+        self.qf1_optimizer = optimizer_class(
+            self.qvf.parameters(),
+            lr=qf_lr,
+        )
+        self.qf2_optimizer = optimizer_class(
+            self.qvf_2.parameters(),
+            lr=qf_lr,
+        )
+        self.vf_optimizer = optimizer_class(
+            self.vf.parameters(),
+            lr=vf_lr,
+        )
+        self.disc_optimizer = optimizer_class(
+            self.discriminator.parameters(),
+            lr=disc_lr
+        )
 
         self.action_dim = self.env.action_space.flat_dim
         self.obs_dim = self.env.observation_space.flat_dim
