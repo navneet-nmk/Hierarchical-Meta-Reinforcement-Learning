5b3b9463ed6e2e1925a05ad36a45a8b2a44fc31d
ayir <navneet.nmk@gmail.com>
2019-01-25 09:58:32 +0100

    Fixed some bugs

--- t_maml_rl.py
+++ t_maml_rl.py
@@ -22,7 +22,8 @@ def seed_def(seed=None):
     return np_random
 
 
-META_POLICY_PATH = "/Users/navneetmadhukumar/Downloads/pytorch-maml-rl/saves/maml/policy-120.pt"
+META_POLICY_PATH = "/Users/navneetmadhukumar/Downloads/pytorch-maml-rl/saves/maml-halfcheetah-dir/policy-999.pt"
+BASELINE_POLICY_PATH = "/Users/navneetmadhukumar/Downloads/pytorch-maml-rl/saves/maml-halfcheetah-dir/baseline.pt"
 
 
 def sample_tasks(num_tasks, seed=None):
@@ -32,8 +33,9 @@ def sample_tasks(num_tasks, seed=None):
     return tasks
 
 
-def load_meta_learner_params(policy_path, env, num_layers=2):
+def load_meta_learner_params(policy_path, baseline_path, env, num_layers=2):
     policy_params = torch.load(policy_path)
+    baseline_params = torch.load(baseline_path)
 
     policy = NormalMLPPolicy(
         int(np.prod(env.observation_space.shape)),
@@ -42,6 +44,7 @@ def load_meta_learner_params(policy_path, env, num_layers=2):
     policy.load_state_dict(policy_params)
 
     baseline = LinearFeatureBaseline(int(np.prod(env.observation_space.shape)))
+    baseline.load_state_dict(baseline_params)
 
     return policy, baseline, policy_params
 
@@ -49,7 +52,6 @@ def load_meta_learner_params(policy_path, env, num_layers=2):
 def evaluate(env, task, policy, policy_params,  max_path_length=100):
     cum_reward = 0
     t = 0
-    #env.reset_task(task)
     obs = env.reset()
     for _ in range(max_path_length):
         obs_tensor = torch.from_numpy(obs).to(device='cpu').type(torch.FloatTensor)
@@ -70,29 +72,32 @@ def evaluate(env, task, policy, policy_params,  max_path_length=100):
 
 def main():
     env = HalfCheetahDirEnv()
-    policy, baseline, params = load_meta_learner_params(META_POLICY_PATH, env, )
+    policy, baseline, params = load_meta_learner_params(META_POLICY_PATH, BASELINE_POLICY_PATH, env, )
     sampler = BatchSampler(env_name='HalfCheetahDir-v1',
                            batch_size=20)
-    learner = MetaLearner(sampler, policy, baseline)
+    gamma = 0.99
+    fast_lr = 0.1
+    tau = 1.0
+    learner = MetaLearner(sampler, policy, baseline,
+                          gamma=gamma,
+                          fast_lr=fast_lr, tau=tau,
+                          )
     writer = SummaryWriter()
 
-    num_updates = 3
+    TEST_TASKS = {'direction': -1.0}
+    num_updates = 5
 
-    cum_reward = 0
-
-    TEST_TASKS = sample_tasks(2, seed=None)
-
-    for i, task in enumerate(TEST_TASKS):
+    for i in range(ITR):
+        task = TEST_TASKS
         print(task)
         env.reset_task(task)
         # Sample a batch of transitions
         sampler.reset_task(task)
         episodes = sampler.sample(policy)
-        for u in range(num_updates):
-            new_params = learner.adapt(episodes)
-            policy.load_state_dict(new_params)
-            cum_reward = evaluate(env, task, policy, params)
-            writer.add_scalar('data/cumm_reward', cum_reward, i)
+        new_params = learner.adapt(episodes)
+        policy.load_state_dict(new_params)
+        cum_reward = evaluate(env, task, policy, params)
+        writer.add_scalar('data/cumm_reward', cum_reward, i)
 
 
 if __name__ == '__main__':
